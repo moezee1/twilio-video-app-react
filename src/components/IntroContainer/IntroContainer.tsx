e0754156feb9aa4f2e23c44810f612f8cc31a7ba
moezee <moe-zee@hotmail.com>
2022-10-17 12:46:54 +0000

    Update 3 files

--- src/components/IntroContainer/IntroContainer.tsx
+++ src/components/IntroContainer/IntroContainer.tsx
@@ -108,7 +108,7 @@ const IntroContainer = (props: IntroContainerProps) => {
             <div className={classes.logoContainer}>
               <VideoLogo />
               <Typography variant="h6" className={classes.title}>
-                Twilio Programmable Video
+                BusinessBadger Video Conferencing
               </Typography>
             </div>
           </div>
